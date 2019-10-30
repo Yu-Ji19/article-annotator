4d10735b90faa2b0b057268f2388a7a507d8f2a8
Hugh White <hughdwhite@gmail.com>
2019-10-30 14:19:43 -0400

    added AnnotationList and Annotation components. began transitioning Annotate to a flexbox layout

--- front-end/src/components/Annotate.js
+++ front-end/src/components/Annotate.js
@@ -5,100 +5,61 @@ import Button from "react-bootstrap/Button"
 import Collaborators from './Collaborators'
 import Share from './Share'
 import Website from './Website'
+import AnnotationList from './AnnotationList'
 
 class Annotate extends Component {
-  state= {
+  state = {
     annotateOn: true
   }
 
   handleClick(e) {
-    if(this.state.annotateOn === true){
-      this.setState({annotateOn: false})
-    }else{
-      this.setState({annotateOn: true})
+    if (this.state.annotateOn === true) {
+      this.setState({ annotateOn: false })
+    } else {
+      this.setState({ annotateOn: true })
     }
   }
 
 
   render() {
+    return (
+      <Container>
+        <div style={{ position: 'absolute', left: '10px' }}>
+          <div>
+            <Share />
+            <br></br>
+          </div>
 
-    if(this.state.annotateOn === true){
-      return (
-        <Container>
-          <div style={{position:'absolute',left:'10px'}}>
-            <div>
-              <Share  />
-              <br></br>
-            </div>
-            
-            <div style={{width: 1100, height: 1000, borderStyle: 'solid', borderWidth: 'thin'}}>
-              <Website />
-            </div>
+          <div style={{ width: '65%', borderStyle: 'solid', borderWidth: 'thin' }}>
+            <Website />
           </div>
-  
-          
-          <div style={{position: 'absolute', right: '0px'}}>
-            <div>
-              <h1>Collaborators</h1>
-              <Collaborators  />
-              <br></br>
-            </div>
-          
-            
-            <Button
-                variant="secondary"
-                onClick={(e) => this.handleClick()}
-              >
-                Annotate On/Off
+        </div>
+
+
+        <div style={{ position: 'absolute', right: '0px' }}>
+          <div>
+            <h1>Collaborators</h1>
+            <Collaborators />
+            <br></br>
+          </div>
+
+
+          <Button
+            variant="secondary"
+            onClick={(e) => this.handleClick()}
+          >
+            Annotate On/Off
               </Button>
-            
-            <div >
-              <h1>Annotations</h1>
-              <div style={{width: 300, height: 800, borderStyle: 'solid', borderWidth: 'thin'}}>
-                  Annotations go here
-              </div>
-            </div>
-          
-  
+
+          <div >
+            <AnnotationList />
           </div>
-          
-        </Container>
-      )
-    }  
-    else{
-      return (
-        <Container>
-  
-          <div style={{position:'absolute',left:'10px'}}>
-            <div>
-              <Share  />
-              <br></br>
-            </div>
-            
-            <div style={{width: 1100, height: 1000, borderStyle: 'solid', borderWidth: 'thin'}}>
-              <Website />
-            </div>
-          </div>
-  
-          
-          <div style={{position: 'absolute', right: '0px'}}>
-            <div>
-              <h1>Collaborators</h1>
-              <Collaborators  />
-              <br></br>
-            </div>
-  
-            <Button
-                variant="secondary"
-                onClick={(e) => this.handleClick()}
-              >
-                Annotate On/Off
-              </Button>
-  
-          </div>   
-        </Container>
-      )
-    }
+
+
+        </div>
+
+      </Container>
+    )
   }
 }
 
