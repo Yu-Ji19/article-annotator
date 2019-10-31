031ca410c3a165251d06b70bb958d7903cc8e76c
Hugh White <hughdwhite@gmail.com>
2019-10-31 15:00:33 -0400

    changed annotate.js to workspace.js and added workspace.js

---
+++ front-end/src/components/Workspace.js
@@ -0,0 +1,49 @@
+import React, { Component } from "react"
+import Container from "react-bootstrap/Container"
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
+
+import Collaborators from './Collaborators'
+import Share from './Share'
+import Website from './Website'
+import AnnotationList from './AnnotationList'
+
+class Workspace extends Component {
+	state = {
+		annotateOn: true,
+		legacy: false,
+	}
+
+	handleClick(e) {
+		if (this.state.annotateOn === true) {
+			this.setState({ annotateOn: false })
+		} else {
+			this.setState({ annotateOn: true })
+		}
+	}
+
+	render() {
+		return (
+			<Container>
+				<Row>
+					<Col xs={8}>
+						<Share />
+					</Col>
+					<Col xs={4}>
+						<Collaborators />
+					</Col>
+				</Row>
+				<Row>
+					<Col xs={8}>
+						<Website />
+					</Col>
+					<Col xs={4}>
+						<AnnotationList />
+					</Col>
+				</Row>
+			</Container>
+		)
+	}
+}
+
+export default Workspace
