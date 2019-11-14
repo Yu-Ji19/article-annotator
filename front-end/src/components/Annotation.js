fbc442dfd6a2677fe9b18795b62b7a5d1be74c09
Hugh White <hughdwhite@gmail.com>
2019-11-14 14:39:54 -0500

    changed APIURL envvar to REACT_APP_APIURL

--- front-end/src/components/Annotation.js
+++ front-end/src/components/Annotation.js
@@ -3,7 +3,7 @@ import Container from "react-bootstrap/Container"
 import Form from "react-bootstrap/Form"
 import Button from "react-bootstrap/Button"
 
-const hostname = process.env["APIURL"] || "http://localhost:8080";
+const hostname = process.env["REACT_APP_APIURL"] || "http://localhost:8080";
 
 class Annotation extends Component {
 	constructor(props) {
--- front-end/src/components/Home.js
+++ front-end/src/components/Home.js
@@ -4,7 +4,7 @@ import InputGroup from "react-bootstrap/InputGroup"
 import FormControl from "react-bootstrap/FormControl"
 import Button from "react-bootstrap/Button"
 
-const hostname = process.env["APIURL"] || "http://localhost:8080";
+const hostname = process.env["REACT_APP_APIURL"] || "http://localhost:8080";
 
 class Home extends Component {
 	constructor(props) {
