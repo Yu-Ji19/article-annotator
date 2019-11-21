990bd3703112c26dd24d4d935e997effa2e71503
Hugh White <hughdwhite@gmail.com>
2019-11-20 19:45:34 -0500

    add canvas under workspace for drawing annotation connections

--- front-end/src/components/Workspace.js
+++ front-end/src/components/Workspace.js
@@ -36,7 +36,7 @@ class Workspace extends Component {
 			pendingAnnotation: null,
 			pendingRange: null,
 			color: "gray",
-			selectedAnnotation:null
+			selectedAnnotation: null
 		}
 		this.createAnnotation = this.createAnnotation.bind(this);
 		this.addCollabName = this.addCollabName.bind(this);
@@ -48,91 +48,90 @@ class Workspace extends Component {
 
 	componentDidMount() {
 		req.get(hostname + '/api/workspace/' + this.state.workspace)
-		.then((response) => response.json().then(data => {
-			this.setState({
-				id: this.state.id,
-				date: data.date,
-				original_url: data.original_url,
-				content: data.content
-			});
-		})
-		);
+			.then((response) => response.json().then(data => {
+				this.setState({
+					id: this.state.id,
+					date: data.date,
+					original_url: data.original_url,
+					content: data.content
+				});
+			})
+			);
 
 		req.get(hostname + '/api/annotation/all/' + this.state.workspace)
-		.then((response) => response.json()
-		.then(data => {
-			var annotations = data.annotations;
-			annotations.forEach(annotation=>{
-				var range = new Range();
-				var startNode = document.getElementById(annotation.range.start);
-				var endNode = document.getElementById(annotation.range.end);
-				annotation.range = range;
-				range.setStart(startNode,0);
-				range.setEnd(endNode,0);
-				rangy.highlight(range, annotation.color);
-				rangy.addTarget(range, annotation.id);
-				annotation.collapsed = false;
-			})
-			this.setState({
-				annotations: annotations.map(v => ({...v, finished: true}))
-			});
-		})
-		.then(()=>{
-			this.state.annotations.forEach((annotation)=>{
-				$("#"+annotation.id).click(this.selectAnnotation);
-			})
-			// rangy.addClick(range);
-		})
-		);
+			.then((response) => response.json()
+				.then(data => {
+					var annotations = data.annotations;
+					annotations.forEach(annotation => {
+						var range = new Range();
+						var startNode = document.getElementById(annotation.range.start);
+						var endNode = document.getElementById(annotation.range.end);
+						annotation.range = range;
+						range.setStart(startNode, 0);
+						range.setEnd(endNode, 0);
+						rangy.highlight(range, annotation.color);
+						rangy.addTarget(range, annotation.id);
+						annotation.collapsed = false;
+					})
+					this.setState({
+						annotations: annotations.map(v => ({ ...v, finished: true }))
+					});
+				})
+				.then(() => {
+					this.state.annotations.forEach((annotation) => {
+						$("#" + annotation.id).click(this.selectAnnotation);
+					})
+					// rangy.addClick(range);
+				})
+			);
 
 
 		req.get(hostname + '/api/collaborators/' + this.state.workspace)
-		.then((response) => response.json().then(data => {
-			this.setState({
-				collaborators: data
-			});
-		})
-		);
+			.then((response) => response.json().then(data => {
+				this.setState({
+					collaborators: data
+				});
+			})
+			);
 	}
 
-	addCollabName(name){
+	addCollabName(name) {
 		this.setState({
 			collabName: name,
-			nameSet:true
-		})	
+			nameSet: true
+		})
 	}
 
-	setColor(color){
-		this.setState({color});
+	setColor(color) {
+		this.setState({ color });
 	}
-
 
 	createAnnotation(annotation) {
 		var selectedText;
 		var range;
-		if (window.getSelection) { 
-			selectedText = window.getSelection(); 
-		} 
-		if(selectedText.rangeCount>0){
+		if (window.getSelection) {
+			selectedText = window.getSelection();
+		}
+		if (selectedText.rangeCount > 0) {
 			range = selectedText.getRangeAt(0);
-			if(range.collapsed){
+			if (range.collapsed) {
 				return;
 			}
-			if(range.commonAncestorContainer.id !== "content"){
+			if (range.commonAncestorContainer.id !== "content") {
 				alert("Illegal Annotation Selection");
 				return;
 			}
 			rangy.highlight(range, this.state.color);
 			this.setState({
-				pendingAnnotation:annotation, 
-				pendingRange:range
+				pendingAnnotation: annotation,
+				pendingRange: range
 			})
-		}else{
+		} else {
 			alert("No text selected");
 		}
 	}
 
-	finishAnnotation(annotation){
+	finishAnnotation(annotation) {
 		var range = rangy.compress(this.state.pendingRange);
 		rangy.addTarget(this.state.pendingRange, annotation.id);
 		// rangy.addClick(this.state.pendingRange);
@@ -151,68 +150,70 @@ class Workspace extends Component {
 			// add response validation
 			var newCollaborators = this.state.collaborators;
 			var collabName = this.state.collabName;
-			newCollaborators[collabName] = !this.state.collaborators || !this.state.collaborators[collabName]? 1: newCollaborators[collabName] + 1;
-			
-			$("#"+annotation.id).click(this.selectAnnotation);
+			newCollaborators[collabName] = !this.state.collaborators || !this.state.collaborators[collabName] ? 1 : newCollaborators[collabName] + 1;
+
+			$("#" + annotation.id).click(this.selectAnnotation);
 			this.setState({
-				pendingAnnotation:null,
+				pendingAnnotation: null,
 				collaborators: newCollaborators,
 				pendingRange: null,
 				annotations: [...this.state.annotations, annotation]
 			})
 		});
-		
+
 	}
 
-	selectAnnotation(annotation){
-		if(annotation.type === "click"){
+	selectAnnotation(annotation) {
+		if (annotation.type === "click") {
 			return;
 		}
 		// console.log(annotation);
 		var selected = this.state.selectedAnnotation;
-		if(!selected ){
+		if (!selected) {
 			rangy.addOverlay(annotation.range, annotation.color);
-			this.setState({selectedAnnotation: annotation});
-		}else if(selected.id !== annotation.id){
+			this.setState({ selectedAnnotation: annotation });
+		} else if (selected.id !== annotation.id) {
 			rangy.addOverlay(annotation.range, annotation.color);
 			rangy.removeOverlay(selected.range, selected.color);
-			this.setState({selectedAnnotation: annotation});
-		}else{
+			this.setState({ selectedAnnotation: annotation });
+		} else {
 			rangy.removeOverlay(selected.range, selected.color);
-			this.setState({selectedAnnotation: null});
+			this.setState({ selectedAnnotation: null });
 		}
 	}
 
 	collapseAnnotation(annotation) {
-		this.setState({annotations: this.state.annotations.map((a) => {
-			if (a.id === annotation.id) {
-				a.collapsed = !a.collapsed;
-			}
-			return a;
-		})})
+		this.setState({
+			annotations: this.state.annotations.map((a) => {
+				if (a.id === annotation.id) {
+					a.collapsed = !a.collapsed;
+				}
+				return a;
+			})
+		})
 		console.log(this.state.annotations);
 	}
 
 	render() {
-		var pendingAnnotation = this.state.pendingAnnotation? 
-								<PendingAnnotation
-									name={this.state.collabName}
-									id={uuidv4()}
-									finishAnnotation = {this.finishAnnotation}
-									color = {this.state.color}
-									range={this.state.pendingRange}
-								/>:null;
-		
+		var pendingAnnotation = this.state.pendingAnnotation ?
+			<PendingAnnotation
+				name={this.state.collabName}
+				id={uuidv4()}
+				finishAnnotation={this.finishAnnotation}
+				color={this.state.color}
+				range={this.state.pendingRange}
+			/> : null;
+
 		return (
 			<Container>
 				<Row>
 					<h1>{this.state.original_url}</h1>
 					<Col xs={8}>
-						<Share 
+						<Share
 							urlID={this.state.workspace}
 							content={this.state.content}
 						/>
-						<Website content={this.state.content}/>
+						<Website content={this.state.content} />
 					</Col>
 					<Col xs={4}>
 						<NameInput
@@ -222,24 +223,34 @@ class Workspace extends Component {
 						<ColorSelection
 							onClick={this.setColor}
 						/>
-						<Collaborators 
+						<Collaborators
 							collaborators={this.state.collaborators}
 						/>
 						<CreateButton
 							createAnnotation={this.createAnnotation}
 						/>
-						<AnnotationList 
-							workspace={this.state.workspace} 
-							annotations = {this.state.annotations}
-							selectAnnotation ={this.selectAnnotation}
-							collapseAnnotation = {this.collapseAnnotation}
-						/>	
+						<AnnotationList
+							workspace={this.state.workspace}
+							annotations={this.state.annotations}
+							selectAnnotation={this.selectAnnotation}
+							collapseAnnotation={this.collapseAnnotation}
+						/>
 						{pendingAnnotation}
 					</Col>
 				</Row>
+				<canvas style={canvasStyle} id="connCanv" />
 			</Container>
 		)
 	}
+}
+
+const canvasStyle = {
+	position: "absolute",
+	top: "0",
+	left: "0",
+	zIndex: "-100",
+	width: "100%",
+	height: "100%"
 }
 
 export default Workspace
